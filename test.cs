9a04e3c070189d7d69aa8a3fd875a2ed34e73573
yukiyasuokamoto <11m32120a@gmail.com>
2017-01-21 20:19:42 +0900

    Script test
    5項目の順並べ・逆並べ

---
+++ test.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class test : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+        //配列を初期化
+        int[] array = { 1, 3, 5, 7, 10 };
+
+        //配列の要素全てを表示する
+        for (int i = 0; i < 5; i++) 
+   
+            {
+            Debug.Log(array[i]);
+        }
+        //配列の要素全てを表示する
+        for (int i = 4; i>=0; i--) 
+              {
+            Debug.Log(array[i]);
+        }
+    }
+
+        //Update is called once per frame
+        void Update (){
+       }
+    }
